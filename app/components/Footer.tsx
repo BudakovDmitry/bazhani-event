8c78f4f1d7c14901a6bf51ef386b9abc21cebd9a
dmitriy.budakov <dmitry.budakov@forforce.com>
2024-02-04 22:29:16 +0200

    Add payment section

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__container container">
+                <a href="#" className="footer__link">Created by Budakov Dmytro</a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import './styles/main.scss'
 import MainSection from "@/app/components/MainSection";
 import CashParty from "@/app/components/CashParty";
 import PaymentSection from "@/app/components/PaymentSection";
+import Footer from "@/app/components/Footer";
 
 export default function Home() {
-  return (
-    <main>
-        <MainSection />
-        <CashParty />
-        <PaymentSection />
-    </main>
-  );
+    return (
+        <>
+            <main>
+                <MainSection/>
+                <CashParty/>
+                <PaymentSection/>
+            </main>
+            <Footer/>
+        </>
+    );
 }
