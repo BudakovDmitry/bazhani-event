f23dc00d96c7ab28a67de6c2ad7a94bfe85db50e
dmitriy.budakov <dmitry.budakov@forforce.com>
2024-02-04 23:20:55 +0200

    Add payment section

--- app/components/MainSection.tsx
+++ app/components/MainSection.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import MainPhoto from "@/public/main_photo.png";
+import LaunchCountdown from "@/app/components/LaunchCountDown";
 
 const MainSection = () => {
     return (
         <section className="main_section">
             <div className="main_section__container container">
+                <div className="main_section__date">
+                    <p className="main_section__date_title">До початку залишилось:</p>
+                    <div className="main_section__date_container">
+                        <LaunchCountdown days={8}
+                                         hours={23}
+                                         minutes={55}
+                                         seconds={41}
+                        />
+                    </div>
+                </div>
                 <Image src={MainPhoto} alt="Main photo" className='main_section__photo' priority={true} unoptimized/>
                 <div className="main_section__title_container">
                     <h1 className="main_section__title">Bazhani</h1>
