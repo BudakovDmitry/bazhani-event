3baab02c18dd53a31a2b036269901f350cda5fd4
dmitriy.budakov <dmitry.budakov@forforce.com>
2024-02-04 15:00:35 +0200

    Add cash party section

--- app/components/CashParty.tsx
+++ app/components/CashParty.tsx
@@ -1,7 +1,46 @@
+'use client'
+
+import { motion } from 'framer-motion';
+
 const CashParty = () => {
+    const numberOfElements = 10;
+
     return (
         <section className="cash_party_section">
+            <div className="container">
+                <h2 className="cash_party_section__title"><span
+                    className="cash_party_section__title_green">Cash</span> party</h2>
+                {Array.from({length: numberOfElements}).map((_, index) => {
+                    const randomX = Math.random() * 375;
+                    const randomRotation = (Math.random() - 0.5) * 360;
 
+                    return (
+                        <motion.div
+                            key={index}
+                            style={{
+                                width: '40px',
+                                height: '20px',
+                                position: 'absolute',
+                                left: randomX,
+                                background: `url('https://upload.wikimedia.org/wikipedia/commons/1/1a/USA_100_Dollar_Bill_Series2009_Obverse.png')`,
+                                backgroundSize: 'cover',
+                                rotate: randomRotation,
+                                zIndex: -1
+                            }}
+                            initial={{y: -100, opacity: 0}}
+                            animate={{
+                                y: [-30, 300],
+                                opacity: [0, 1, 0],
+                                transition: {repeat: Infinity, duration: 3, ease: 'linear', delay: index * 0.5}
+                            }}
+                        ></motion.div>
+                    )
+                })}
+                <h3 className='cash_party_section__title_second'>Запрошуємо тебе на найбажаніший івент року про
+                    гроші!</h3>
+                <p className='cash_party_section__description'>Приготуйся до неймовірних вражень та розкішних
+                    можливостей заробітку.</p>
+            </div>
         </section>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import './styles/main.scss'
 import MainSection from "@/app/components/MainSection";
+import CashParty from "@/app/components/CashParty";
 
 export default function Home() {
   return (
     <main>
         <MainSection />
+        <CashParty />
     </main>
   );
 }
